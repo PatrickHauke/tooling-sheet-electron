a93b8b3b8d95eaeb04a80b049ef299dab2f55d03
Patrick <patrick.hauke@outlook.com>
2018-04-29 23:11:13 -0400

    data ipc refactor

--- ipc/formSubmit.js
+++ ipc/formSubmit.js
@@ -1,17 +1,20 @@
-// const { ipcRenderer } = require('electron');
-// document.getElementById('render').addEventListener('click', () => {
-//     const inputs = document.querySelectorAll('.hook');
-//     let formData = [];
-//     inputs.forEach((el) => {
-//         formData.push(
-//             {
-//                 "name": el.name,
-//                 "data": el.value
-//             }
-//         )
-//     });
+const { ipcRenderer } = require('electron');
+document.getElementById('render').addEventListener('click', () => {
+    const spindleData = $('.hook');
+    let dataSet = [];
+    for(var i = 0; i<spindleData.length; i++){
+        dataSet.push({
+            'name': spindleData[i].name,
+            'value': spindleData[i].value
+        });
+    }
 
-//     ipcRenderer.send('update-second-page', formData);
-//     // Trigger event listener action in render process (second.html) from main process
-//     // ipcRenderer.send('update-second-page', Data);
-// }, false);
+    // console.log(dataSet);
+    // spindleData[0].foreach((data)=>{
+    //     console.log(data);
+    // })
+
+    ipcRenderer.send('update-second-page', dataSet);
+    // Trigger event listener action in render process (second.html) from main process
+    // ipcRenderer.send('update-second-page', Data);
+}, false);
